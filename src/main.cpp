31ed374b64fe9aef1c7453ff53380a206d39faad
jakerwalters <jakerw3@illinois.edu>
2019-11-19 17:01:58 -0600

    Add all default project generated methods to ofApp header and cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,14 @@
 
 using json = nlohmann::json;
 
-#define DISPLAY_MODE OF_WINDOW  // Can be OF_WINDOW or OF_FULLSCREEN
+#define DISPLAY_MODE OF_WINDOW
 
 int main() {
 	json j;
-
-	ofSetupOpenGL(640, 480, DISPLAY_MODE);  // setup the GL context
-	ofSetFrameRate(
-			12);  // An appropriate framerate that moves the snake at a good speed
-						// Due to the simple nature of our objects rendering things this
-						// slow should not cause visual discomfort or lage
+	
+	// setup the GL context
+	ofSetupOpenGL(640, 480, DISPLAY_MODE);
 
 	// this kicks off the running of my app
-	ofRunApp();
+	ofRunApp(new ofApp());
 }
