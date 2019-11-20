9e6695202e69280a5b0416b1bb8b1ae6717d292c
jakerwalters <jakerw3@illinois.edu>
2019-11-19 21:58:23 -0600

    Link libcurl library test query of demo data from API

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include "ofApp.h"
 #include "ofMain.h"
 #include "json.hpp"
+#include <curl/curl.h>
 
 using json = nlohmann::json;
 
 #define DISPLAY_MODE OF_WINDOW
 
 int main() {
+	
+	CURL *curl = curl_easy_init();
+	if(curl) {
+		CURLcode res;
+		curl_easy_setopt(curl, CURLOPT_URL, "https://api.worldtradingdata.com/api/v1/mutualfund?symbol=AAAAX,AAADX,AAAGX&api_token=demo");
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
+	
 	json j;
 	
 	// setup the GL context
