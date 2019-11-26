94c52ad4d58d2d892afb7c89d4fbdf095cc38547
jakerwalters <jakerw3@illinois.edu>
2019-11-25 20:19:43 -0600

    Add catch header and new target for testing

---
+++ tests/testing.cpp
@@ -0,0 +1,12 @@
+//
+//  testing.cpp
+//  fantastic-finale-tests
+//
+//  Created by Jake Walters on 11/25/19.
+//
+
+#include "catch.hpp"
+
+TEST_CASE("sample test") {
+	REQUIRE(3 == 3);
+}
---
+++ tests/tests-main.cpp
@@ -0,0 +1,10 @@
+//
+//  tests-main.cpp
+//  fantastic-finale-Jakerwalters copy
+//
+//  Created by Jake Walters on 11/25/19.
+//
+
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE
+#include "catch.hpp"
